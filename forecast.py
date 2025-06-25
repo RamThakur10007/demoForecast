f5054dfa9dd83153200173e30f7fb5a111e40557
Ram Yogesh Thakur <97776580+RamThakur10007@users.noreply.github.com>
2025-06-25 15:23:34 +0530

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -87,6 +87,6 @@ def get_forecast():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Render provides PORT env variable
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("forecast:app", host="0.0.0.0", port=port, reload=False)
 
  

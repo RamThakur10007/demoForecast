75d0fa460294ebcc3019662af901330e623d1007
Ram Yogesh Thakur <97776580+RamThakur10007@users.noreply.github.com>
2025-06-25 13:34:01 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec ${UVCORN_CMD:-"uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4 --log-level info"}
+exec ${UVCORN_CMD:-"uvicorn forecast:app --host 0.0.0.0 --port 8000 --workers 4 --log-level info"}
